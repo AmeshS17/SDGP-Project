fc3d5db30ed58b0519b7b2e0a49c3226eccbe278
AmeshS17 <sivakumar.2019196@iit.ac.lk>
2021-04-14 03:51:21 +0530

    Added backend function scripts

---
+++ Backend/Lambda_Functions/generate_s3_upload_url/generate_s3_upload_url.py
@@ -0,0 +1,28 @@
+import json
+import boto3
+import uuid
+from botocore.client import Config
+
+def lambda_handler(event, context):
+    # TODO implement
+    
+    print("Invocation event - " + str(event))
+    
+    s3 = boto3.client('s3', 'us-west-2', config=Config(s3={'addressing_style': 'path'}))
+    
+    # Generate a random S3 key name
+    upload_key = uuid.uuid4().hex+".csv"
+
+    # Generate the presigned URL for put requests
+    presigned_url = s3.generate_presigned_url(
+        ClientMethod='put_object',
+        Params={
+            'Bucket': 'raw-csv-files',
+            'Key': upload_key
+        }
+    )
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps({"upload_url": presigned_url,"file_key":upload_key[:-3]+"json"})
+    }
---
+++ Backend/Lambda_Functions/preprocess_text/preprocess_text.py
@@ -0,0 +1,90 @@
+import json
+import urllib.parse
+import boto3
+import csv
+import io
+
+s3 = boto3.client('s3')
+lambda_client = boto3.client('lambda')
+
+def get_csv(bucket,key):
+    
+    csv_object = s3.get_object(Bucket=bucket, Key=key)
+    csv_content = csv_object['Body'].read().decode('utf-8').split('\n')
+    
+    csv_dict = list(csv.DictReader(csv_content))
+    
+    print(csv_dict[:15])
+    print("Data dictionary length : " + str(len(csv_dict)))
+    
+    return csv_dict
+
+
+def put_csv(csv_dict,bucket,key):
+    csv_headers = csv_dict[0].keys()
+    
+    filestream = io.StringIO()
+    
+    writer = csv.DictWriter(filestream, csv_headers)
+    
+    writer.writeheader()
+    writer.writerows(csv_dict)
+    
+    csv_object = filestream.getvalue()
+    
+    response = s3.put_object(Body=csv_object,Bucket=bucket,Key=key)
+
+    print(response)
+    return True
+
+
+def invoke_model(key):
+    response = lambda_client.invoke(
+        FunctionName = 'generate_summary',
+        InvocationType = 'Event',
+        Payload = json.dumps({'filekey':key})
+    )
+
+
+def lambda_handler(event, context):
+
+    print("Invocation event - " + str(event))
+
+    # Get the object from the event and show its content type
+    get_bucket = event['Records'][0]['s3']['bucket']['name']
+    put_bucket = 'cleaned-csv-files'
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    
+    waiter = s3.get_waiter('object_exists')
+    waiter.wait(
+        Bucket=get_bucket,
+        Key=key,
+        WaiterConfig={
+            'Delay': 10,
+            'MaxAttempts':15
+        }
+    )
+    
+    try:
+        data_dict = get_csv(get_bucket,key)
+        
+        
+        uploaded = put_csv(data_dict,put_bucket,key)
+        
+        waiter.wait(
+        Bucket=put_bucket,
+        Key=key,
+        WaiterConfig={
+            'Delay': 10,
+            'MaxAttempts':15
+            }
+        )
+    
+        invoke_model(key)
+        return None
+    except Exception as e:
+        print(e)
+        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, get_bucket))
+        raise e
+
+
