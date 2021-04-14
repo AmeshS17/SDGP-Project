a57ebf1fcec1533ad1c599d711502cd8b19076e8
AmeshS17 <sivakumar.2019196@iit.ac.lk>
2021-04-15 05:26:35 +0530

    Added upload session test

--- Backend/Tests/test_upload_session/test_upload_session.py
+++ Backend/Tests/test_upload_session/test_upload_session.py
@@ -1,12 +1,48 @@
 import requests
+import time
 
-def test_get_game_list(api_url):
-    response = requests.get(api_url)
+
+def get_upload_url(api_url):
+    response = requests.get(api_url+"upload-url")
     assert response.status_code == 200
-    print ('test_get_game_list passed')
-    print(response.text)
+    print ('get_upload_url passed')
+    print("get_upload_url response = " + str(response.json()))
+    return response.json()
 
 
-base_url = 'https://8k7ni4cse1.execute-api.us-west-2.amazonaws.com/github-deploy-test/games'
+def upload_file(upload_url,file_path):
+    with open(file_path,"rb") as file:
+        response = requests.put(upload_url,data=file)
+    assert response.status_code == 200
+    print ('upload_file passed')
+    print(file.name + " has been uploaded to S3")
 
-test_get_game_list(base_url)
+
+def get_summary(base_url,file_key):
+    payload = {"filekey":file_key}
+    for i in range(6):
+        response = requests.post(base_url+"results",json=payload)
+        lambda_status_code = int(response.json()['statusCode'])
+        if lambda_status_code == 204:
+            time.sleep((i+1)*5)
+            continue
+        if lambda_status_code == 200:
+            print ('get_summary passed')
+            print("get_summary request {} response = ".format(i) + str(response.json()))
+            return response.json()
+    #Raise an exception if results were not returned after 6 tries
+    raise Exception("Results were not returned after requesting 6 times")
+    
+
+base_url = 'https://8k7ni4cse1.execute-api.us-west-2.amazonaws.com/github-deploy-test/'
+
+
+
+upload_url_response = get_upload_url(base_url) 
+upload_url = upload_url_response['uploadurl']
+file_key = upload_url_response['filekey']
+
+file_path = "./Backend/Tests/test_upload_session/testfile.csv"
+
+upload_file(upload_url,file_path)
+summary_response = get_summary(base_url,file_key)
