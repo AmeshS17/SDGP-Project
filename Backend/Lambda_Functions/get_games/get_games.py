34313ab7b9f03c5aa87dffdaa27f3f5292e4ede9
Amesh Sivakumar <70102453+AmeshS17@users.noreply.github.com>
2021-04-16 03:53:23 +0530

    Update get_games.py

--- Backend/Lambda_Functions/get_games/get_games.py
+++ Backend/Lambda_Functions/get_games/get_games.py
@@ -39,6 +39,11 @@ def lambda_handler(event, context):
     
     return {
         'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': 'https://www.example.com',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        },
         'body': json.dumps(response)
     }
     
