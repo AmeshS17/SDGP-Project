ce78501f68284f4b085932eadb6c352657b1cac4
Amesh Sivakumar <70102453+AmeshS17@users.noreply.github.com>
2021-04-16 18:12:35 +0530

    Update test_upload_session.py

--- Backend/Tests/test_upload_session/test_upload_session.py
+++ Backend/Tests/test_upload_session/test_upload_session.py
@@ -4,15 +4,16 @@ import time
 
 def get_upload_url(api_url):
     response = requests.get(api_url+"upload-url")
+    print("get_upload_url response = " + str(response.json()))
     assert response.status_code == 200
     print ('get_upload_url passed')
-    print("get_upload_url response = " + str(response.json()))
     return response.json()
 
 
 def upload_file(upload_url,file_path):
     with open(file_path,"rb") as file:
         response = requests.put(upload_url,data=file)
+    print( "upload_file response status" + str(response.status_code))
     assert response.status_code == 200
     print ('upload_file passed')
     print(file.name + " has been uploaded to S3")
@@ -22,6 +23,7 @@ def get_summary(base_url,file_key):
     payload = {"filekey":file_key}
     for i in range(6):
         response = requests.post(base_url+"results",json=payload)
+        print("get_summary request {} response = ".format(i) + str(response.json()))
         assert response.status_code == 200
         lambda_status_code = int(response.json()['statusCode'])
         if lambda_status_code == 204:
@@ -29,7 +31,6 @@ def get_summary(base_url,file_key):
             continue
         if lambda_status_code == 200:
             print ('get_summary passed')
-            print("get_summary request {} response = ".format(i) + str(response.json()))
             return response.json()
     #Raise an exception if results were not returned after 6 tries
     raise Exception("Results were not returned after requesting 6 times")
