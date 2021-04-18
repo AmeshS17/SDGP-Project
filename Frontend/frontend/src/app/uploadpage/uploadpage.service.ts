5a91724d1b473a58dfe9574faefbe3cd7afaa3c1
Chathma <chathz07@gmail.com>
2021-04-18 18:03:10 +0530

    upload implementation

--- Frontend/frontend/src/app/uploadpage/uploadpage.service.ts
+++ Frontend/frontend/src/app/uploadpage/uploadpage.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import {url_response} from './uploadpage.model';
 
 import { Observable, throwError } from 'rxjs';
@@ -21,14 +21,21 @@ export class UploadpageService {
   constructor(private http: HttpClient) { }
 
   getUploadUrl() : Observable<url_response>{
-    return this.http.get<GetResponse>(this.baseurl).pipe(
-      map(response => response.url)
-    );
+    return this.http.get<url_response>(this.baseurl)
+    //.pipe(map(response => response.url));
 
   }
-}
 
-interface GetResponse{
-  url : url_response;
+  uploadFile(file:File, uploadurl:string): Observable<boolean> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'text/csv'
+      })
+    }
+    const formData: FormData = new FormData();
+    formData.append('fileKey', file, file.name);
+    return this.http
+      .put(uploadurl, formData, httpOptions)
+      .pipe(map(() => {return true; }));
 }
-
+}
