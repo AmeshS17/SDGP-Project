bb2af121a0cde143aa32c4fecb611e2ce3bf84b3
Amesh Sivakumar <70102453+AmeshS17@users.noreply.github.com>
2021-04-19 02:03:29 +0530

    Update retrieve_summary.py

--- Backend/Lambda_Functions/retrieve_summary/retrieve_summary.py
+++ Backend/Lambda_Functions/retrieve_summary/retrieve_summary.py
@@ -34,7 +34,7 @@ def lambda_handler(event, context):
         return {
             'statusCode': 200,
             'headers': {
-                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Headers': '*',
                 'Access-Control-Allow-Origin': '*',
                 'Access-Control-Allow-Methods': 'OPTIONS,GET'
             },
@@ -47,7 +47,7 @@ def lambda_handler(event, context):
         return {
             'statusCode': 204,
             'headers': {
-                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Headers': '*',
                 'Access-Control-Allow-Origin': '*',
                 'Access-Control-Allow-Methods': 'OPTIONS,GET'
             },
